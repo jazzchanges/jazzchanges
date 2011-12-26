04754479c2fa02d3f2e7bef411bd61838b45a0c1
Bryan Helmig <bryan@bryanhelmig.com>
2011-12-26 01:48:02 -0600

    gitignore, postgres

--- jazzchanges/urls.py
+++ jazzchanges/urls.py
@@ -8,7 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
-
     url(r'^accounts/', include('jazzchanges.customuser.urls')),
     url(r'^accounts/', include('userena.urls')),
 
